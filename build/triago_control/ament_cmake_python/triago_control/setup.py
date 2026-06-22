f88d2be4e4bcc14299459460e917f266422249d7
Robertorocco <roby.rocco01@gmailcom>
2026-06-22 08:37:12 +0000

    Tune grip closure

--- build/triago_control/ament_cmake_python/triago_control/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='triago_control',
-    version='0.0.1',
-    packages=find_packages(
-        include=('triago_control', 'triago_control.*')),
-)
